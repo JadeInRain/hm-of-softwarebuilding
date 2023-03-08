df536213c30ebce0267f73f2ca2ada5e030766f6
JadeInRain <92285538+JadeInRain@users.noreply.github.com>
2023-03-08 18:40:13 +0800

    Update Program.cs

--- Assignment1/ConsoleCalculator/Program.cs
+++ Assignment1/ConsoleCalculator/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,40 +11,43 @@ namespace assignment1
         static void Main(string[] args)
         {
             int a;
+            Console.WriteLine("请输入第一个数并换行：");
             int.TryParse(Console.ReadLine(), out a);
 
             char b;
+            Console.WriteLine("请输入操作符并换行：");
             char.TryParse(Console.ReadLine(), out b);
 
             int c;
+            Console.WriteLine("请输入第二个数并换行：");
             int.TryParse(Console.ReadLine(), out c);
 
             switch (b)
             {
                 case '+':
-                    Console.WriteLine("=");
+                    Console.WriteLine("结果：");
                     Console.WriteLine(a + c);
                     Console.WriteLine("");
                     break;
                 case '-':
-                    Console.WriteLine("=");
+                    Console.WriteLine("结果：");
                     Console.WriteLine(a - c);
                     Console.WriteLine("");
                     break;
                 case '*':
-                    Console.WriteLine("=");
+                    Console.WriteLine("结果：");
                     Console.WriteLine(a * c);
                     Console.WriteLine("");
                     break;
                 case '/':
-                    Console.WriteLine("=");
+                    Console.WriteLine("结果：");
                     Console.WriteLine(a / c);
                     Console.WriteLine("");
                     break;
                 default:
-                    Console.WriteLine("Invalid Expression");
+                    Console.WriteLine("Error");
                     break;
-                    Console.ReadKey();
+                    //Console.ReadKey();
             }
         }
     }
