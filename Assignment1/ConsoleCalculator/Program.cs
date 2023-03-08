970995f62a9bc1eab9122019ef4dba9f903a1e83
JadeInRain <92285538+JadeInRain@users.noreply.github.com>
2023-03-08 16:54:25 +0800

    Add files via upload

---
+++ Assignment1/ConsoleCalculator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int.TryParse(Console.ReadLine(), out a);
+
+            char b;
+            char.TryParse(Console.ReadLine(), out b);
+
+            int c;
+            int.TryParse(Console.ReadLine(), out c);
+
+            switch (b)
+            {
+                case '+':
+                    Console.WriteLine("=");
+                    Console.WriteLine(a + c);
+                    Console.WriteLine("");
+                    break;
+                case '-':
+                    Console.WriteLine("=");
+                    Console.WriteLine(a - c);
+                    Console.WriteLine("");
+                    break;
+                case '*':
+                    Console.WriteLine("=");
+                    Console.WriteLine(a * c);
+                    Console.WriteLine("");
+                    break;
+                case '/':
+                    Console.WriteLine("=");
+                    Console.WriteLine(a / c);
+                    Console.WriteLine("");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Expression");
+                    break;
+                    Console.ReadKey();
+            }
+        }
+    }
+}
