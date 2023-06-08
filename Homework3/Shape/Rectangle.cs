813a508fe0cd4e3573219e4ddb203a15a373b9fa
JadeInRain <92285538+JadeInRain@users.noreply.github.com>
2023-06-09 01:01:45 +0800

    Add files via upload

---
+++ Homework3/Shape/Rectangle.cs
@@ -0,0 +1,45 @@
+﻿namespace Shape
+{
+    public class Rectangle : Shape, IsLegal
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Area { get; private set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public bool IsShapeLegal()
+        {
+            if (Width > 0 && Height > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        //形状不合法
+        override public double AreaCalculate()
+        {
+            if (IsShapeLegal())
+            {
+                return Area = Width * Height;
+            }
+
+            return Area = -1;
+        }
+
+        public override string ToString()
+        {
+            if (Area != -1)
+            {
+                return "Rectangle(Legal  Width = " + Width + " Height = " + Height + " Area = " + Area + ")";
+            }
+
+            return "Rectangle(Illegal  Width = " + Width + " Height = " + Height + ")";
+        }
+    }
+}
---
+++ Homework3/Shape/ShapeFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Shape
+{
+    public class ShapeFactory
+    {
+        //随机生成长宽高边长等属性
+        private Random rand;
+
+        public ShapeFactory()
+        {
+            rand = new Random();
+        }
+        public Shape GetShape(string shapeType)
+        {
+            if (shapeType == null)
+            {
+                return null;
+            }
+
+            if (shapeType.Equals("RECTANGLE", StringComparison.OrdinalIgnoreCase))
+            {
+                return new Rectangle(rand.Next(21), rand.Next(21));
+            }
+            else if (shapeType.Equals("SQUARE", StringComparison.OrdinalIgnoreCase))
+            {
+                return new Square(rand.Next(21));
+            }
+            else if (shapeType.Equals("TRIANGLE", StringComparison.OrdinalIgnoreCase))
+            {
+                return new Triangle(rand.Next(21), rand.Next(21), rand.Next(21));
+            }
+
+            return null;
+        }
+    }
+}
